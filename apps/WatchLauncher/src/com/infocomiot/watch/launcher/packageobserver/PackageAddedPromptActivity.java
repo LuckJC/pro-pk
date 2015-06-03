37e34952d62b0ccdc21b71fef7fe542934db804d
lihaijiang <lihaijiang@infocomiot.com>
2015-06-03 10:19:57 +0800

    桌面：弹出新增应用对话框时，按返回键会添加到普通应用列表

--- apps/WatchLauncher/src/com/infocomiot/watch/launcher/packageobserver/PackageAddedPromptActivity.java
+++ apps/WatchLauncher/src/com/infocomiot/watch/launcher/packageobserver/PackageAddedPromptActivity.java
@@ -25,6 +25,7 @@ import com.infocomiot.watch.launcher.provider.LauncherConfig;
  */
 public class PackageAddedPromptActivity extends Activity implements OnClickListener {
 	private String mPackageName;
+	private boolean mAddToSportsGroup = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -65,18 +66,13 @@ public class PackageAddedPromptActivity extends Activity implements OnClickListe
 
 	@Override
 	public void onClick(View v) {
-		ContentValues values = new ContentValues();
-		values.put(LauncherConfig.PACKAGE_NAME, mPackageName);
-		
 		switch (v.getId()) {
 		case R.id.ok:
-			values.put(LauncherConfig.PACKAGE_TYPE, LauncherConfig.TYPE_SPORTS);
-			getContentResolver().insert(LauncherConfig.WORKSPACE_CONTENT_URI, values);
+			mAddToSportsGroup = true;
 			break;
 			
 		case R.id.cancel:
-			values.put(LauncherConfig.PACKAGE_TYPE, LauncherConfig.TYPE_COMMON);
-			getContentResolver().insert(LauncherConfig.WORKSPACE_CONTENT_URI, values);
+			mAddToSportsGroup = false;
 			break;
 
 		default:
@@ -84,5 +80,19 @@ public class PackageAddedPromptActivity extends Activity implements OnClickListe
 		}
 		
 		finish();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		ContentValues values = new ContentValues();
+		values.put(LauncherConfig.PACKAGE_NAME, mPackageName);
+		if (mAddToSportsGroup) {
+			values.put(LauncherConfig.PACKAGE_TYPE, LauncherConfig.TYPE_SPORTS);
+			getContentResolver().insert(LauncherConfig.WORKSPACE_CONTENT_URI, values);
+		} else {
+			values.put(LauncherConfig.PACKAGE_TYPE, LauncherConfig.TYPE_COMMON);
+			getContentResolver().insert(LauncherConfig.WORKSPACE_CONTENT_URI, values);
+		}
 	}
 }
